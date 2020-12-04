c10708f83aa0c9340fe583892685dae5a2aaf3b8
Yusuf <yusufyilmazfr@gmail.com>
2020-12-04 13:34:15 +0300

    I added new custom load balancer and request rating configuration section.

---
+++ src/OcelotApiGateway.GatewayApi/CustomLoadBalancers/CustomLoadBalancer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Ocelot.LoadBalancer.LoadBalancers;
+using Ocelot.Responses;
+using Ocelot.Values;
+
+namespace OcelotApiGateway.GatewayApi.CustomLoadBalancers
+{
+    public class CustomLoadBalancer : ILoadBalancer
+    {
+        private readonly Func<Task<List<Service>>> _services;
+        private readonly object _lock = new object();
+
+        private int _last;
+
+        public CustomLoadBalancer(Func<Task<List<Service>>> services)
+        {
+            _services = services;
+        }
+
+        public async Task<Response<ServiceHostAndPort>> Lease(HttpContext httpContext)
+        {
+            var services = await _services();
+
+            lock (_lock)
+            {
+                if (_last >= services.Count)
+                {
+                    _last = 0;
+                }
+
+                var next = services[_last];
+
+                _last++;
+
+                return new OkResponse<ServiceHostAndPort>(next.HostAndPort);
+            }
+        }
+
+        public void Release(ServiceHostAndPort hostAndPort)
+        {
+
+        }
+    }
+}
--- src/OcelotApiGateway.GatewayApi/Startup.cs
+++ src/OcelotApiGateway.GatewayApi/Startup.cs
@@ -10,8 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ocelot.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.ServiceDiscovery.Providers;
+using OcelotApiGateway.GatewayApi.CustomLoadBalancers;
 
 namespace OcelotApiGateway.GatewayApi
 {
@@ -29,7 +32,11 @@ namespace OcelotApiGateway.GatewayApi
         {
             services.AddControllers();
 
-            services.AddOcelot();
+            Func<IServiceProvider, DownstreamRoute, IServiceDiscoveryProvider, CustomLoadBalancer> loadBalancerFactoryFunc = (serviceProvider, Route, serviceDiscoveryProvider) => new CustomLoadBalancer(serviceDiscoveryProvider.Get);
+
+
+            services.AddOcelot()
+                .AddCustomLoadBalancer(loadBalancerFactoryFunc);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
